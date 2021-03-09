b857ee9ba2eb91739a8f71f4088d8689cdfa5a8c
Diana Kondakova <greys.mouse@yandex.ru>
2021-03-09 18:31:43 +0300

    Sprint 8 iteration (1)

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -1,13 +1,17 @@
 import {Popup} from './Popup.js';
 
 class PopupWithImage extends Popup {
-  open(name, link) {
-    const image = this._popupElement.querySelector('.popup__place-image');
-    const caption = this._popupElement.querySelector('.popup__place-caption');
+  constructor(popupSelector) {
+    super(popupSelector);
+    
+    this._cardImage = this._popupElement.querySelector('.popup__place-image');
+    this._cardCaption = this._popupElement.querySelector('.popup__place-caption');
+  }
 
-    image.src = link;
-    image.alt = name;
-    caption.textContent = name;
+  open(name, link) {
+    this._cardImage.src = link;
+    this._cardImage.alt = name;
+    this._cardCaption.textContent = name;
     
     super.open();
   }
